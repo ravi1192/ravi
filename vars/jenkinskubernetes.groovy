d5fbb4795a6146786e3b078e59ebd2348c1acb0d
Ravindra <ravindratamshatte.com>
2023-10-02 23:54:43 +0530

    Added files

---
+++ vars/jenkinskubernetes.groovy
@@ -0,0 +1,52 @@
+def call(String registryCred = 'a', String registryin = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a') {
+
+pipeline {
+environment { 
+		registryCredential = "${registryCred}"
+		registry = "$registryin" 	
+		dockerTag = "${docTag}_$BUILD_NUMBER"
+		gitRepo = "${grepo}"
+		gitBranch = "${gbranch}"
+		gitCredId = "${gitcred}"
+	}
+		
+	agent { label 'slave1' }
+	
+	triggers {
+		pollSCM '* * * * *'
+	}
+
+	stages {
+		stage("POLL SCM"){
+			steps {
+				 checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])
+			}
+		}	
+					
+		stage('BUILD IMAGE') { 
+			 steps { 
+				 script { 
+					 dockerImage = docker.build('"$registry:$dockerTag"') 
+				 }
+			} 
+		}
+					
+		stage('PUSH HUB') { 
+			 steps { 
+				 script { 
+					 docker.withRegistry( '', "$registryCredential" ) { 
+						 dockerImage.push() 
+					}
+				}		
+			} 
+		}
+					
+		stage('DEPLOY IMAGE') {
+			steps {
+				sh 'kubectl set image deployment/webapp-deployment nodejs="$registry:$dockerTag" --record'
+			}
+		}
+	}
+			  
+}
+}
